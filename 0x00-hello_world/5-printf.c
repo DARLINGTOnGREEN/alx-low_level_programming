4cc429a0fe4697a7897288647c587f636672534d
DARLINGTON GREEN <darlintongreen40@gmail.com>
2023-09-07 18:20:21 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
-include <stdio.h>
-/** main - delcare main function
- *
- *  Return: always 0
- *
+#include <stdio.h>
+/**
+ * main - main block of code
+ * Return: 0
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0)
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
