c18982980ee8138217f78a205e25578e2f3ea9a7
DARLINGTON GREEN <darlintongreen40@gmail.com>
2023-09-11 09:30:46 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print letters in lowercase
+ *
+ * return: always 0.
+ */
+int main(void)
+{
+	char b;
+	for(b = 'a'; b <='z'; b++)
+		putchar(b);
+	putchar('\n');
+	
+
+	return (0);
+
+}
+	
