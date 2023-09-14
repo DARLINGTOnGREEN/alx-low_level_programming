57be84c6c7e75f58801c524b39d84c0379294201
DARLINGTOnGREEN <darlingtongreen40@.com>
2023-09-14 17:11:46 +0000

    11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - prints number fron n to 98.
+ * @n: number.
+ * Return: 0.
+ */
+void print_to_98(int n)
+{
+	int n;
+
+	for (n = 0; n <= 98; n++)
+		printf("%d" , n);
+		putchar('\n');
+}
