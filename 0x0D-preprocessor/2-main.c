452a00ed9ada44982b46812605670eb4936ba0bb
DARLINGTON GREEN <darlintongreen40@gmail.com>
2023-10-21 19:42:39 -0700

    update

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+
 /**
- * main - print name of file
- * Return: 0
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
  */
+
 int main(void)
 {
-	printf("%s\n", _FILE_);
+	printf("%s\n", __FILE__);
 	return (0);
 }
