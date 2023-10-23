2b84253be6a3255d06cdbd2bc3da684a281d358f
DARLINGTON GREEN <darlintongreen40@gmail.com>
2023-10-23 13:57:38 -0700

    update

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_listint - function that prints all the elements of a listint_t
+ * @h: head of the list
+ *
+ * Return: the number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	const listint_t *chips = h;
+	size_t count = 0;
+
+	while (cursor != NULL)
+	{
+		printf("%d\n", chips->n);
+		count += 1;
+		chips = chips->next;
+	}
+	return (count);
+}
