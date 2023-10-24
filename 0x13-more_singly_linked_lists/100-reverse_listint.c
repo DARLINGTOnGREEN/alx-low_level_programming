b79c295b7a9b2c97f1ca0c159342ebd530b17725
DARLINGTON GREEN <darlintongreen40@gmail.com>
2023-10-24 14:36:50 -0700

    upload

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - reverses a linked list
+ * @head: head of the list
+ *
+ * Return: the first node of the reversed node
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *forward_node = NULL;
+	listint_t *backward_node = NULL;
+
+	if (head == NULL)
+		return (NULL);
+	while (*head != NULL)
+	{
+		forward = (*head)->next;
+		(*head)->next = backward_node;
+		backward_node = *head;
+		*head = forward_node;
+	}
+	*head = backward_node;
+	return (*head);
+}
