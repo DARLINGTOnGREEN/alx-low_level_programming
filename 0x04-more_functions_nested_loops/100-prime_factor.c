ed69ba5ad68390df92809420c3b0d235b4f0d9d1
DARLINGTOnGREEN <darlingtongreen4@gmail.com>
2024-07-30 16:59:16 +0100

    up

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -6,24 +6,26 @@
  *
  * Return: returns 0
  */
-
 int main(void)
 {
-    long number = 612852475143;
-    long factor = 2;
+	long number = 612852475143;
+	long factor = 2;
+	long largest_factor = 0;
 
-    while (factor <= number)
-    {
-        if (number % factor == 0)
-        {
-            number /= factor;
-        }
-        else
-        {
-            factor++;
-        }
-    }
+	while (number > 1)
+	{
+		if (number % factor == 0)
+		{
+			largest_factor = factor;
+			number /= factor;
+			while (number % factor == 0)
+			{
+				number /= factor;
+			}
+		}
+		factor++;
+	}
 
-    printf("%ld\n", number);
-    return 0;
+	printf("%ld\n", largest_factor);
+	return (0);
 }
